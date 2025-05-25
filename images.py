903f3a4275d568141bc7915733611a977dfda2c0
korneliuspgk <korneliusplaygames@gmail,com>
2025-05-25 17:42:42 +0200

    loadign image

--- images.py
+++ images.py
@@ -1,2 +1,14 @@
-import dearpygui.dearpygui as dpg
 import numpy as np
+from PIL import Image
+
+
+
+class ImageLoader:
+    def __init__(self, path):
+        self.path = path
+        self.image_array = None
+
+    def load_grayscale(self):
+        image = Image.open(self.path).convert("L")
+        self.image_array = np.array(image, dtype=np.uint8)
+        return self.image_array
