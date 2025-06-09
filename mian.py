e6062b820d4a8d2d2bac4811bc3dd08c5ac878ec
korneliuspgk <korneliusplaygames@gmail,com>
2025-06-09 13:37:49 +0200

    main test file

--- mian.py
+++ mian.py
@@ -7,11 +7,14 @@ loader = ImageLoader("obraz.bmp")
 img = loader.load_grayscale()
 
 # Filtry
-blurred = ImageFilters.gaussian_blur(img, size=5, sigma=1.0)
-medianed = ImageFilters.median_filter(img, size=3)
-equalized = ImageFilters.histogram_equalization(img)
+#blurred = ImageFilters.gaussian_blur(img, size=5, sigma=1.0)
+#medianed = ImageFilters.median_filter(img, size=3)
+#equalized = ImageFilters.histogram_equalization(img)
+edge = ImageFilters.edge_detection(img, force=2)
+
 
 # Zapis wyników
-Image.fromarray(blurred).save("gauss.png")
-Image.fromarray(medianed).save("median.png")
-Image.fromarray(equalized).save("equalized.png")
+#Image.fromarray(blurred).save("gauss.png")
+#Image.fromarray(medianed).save("median.png")
+#Image.fromarray(equalized).save("equalized.png")
+Image.fromarray(edge).save("edge.png")
