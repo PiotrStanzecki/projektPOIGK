9a6823ed060171aba3364caed0653ffbef58b229
korneliuspgk <korneliusplaygames@gmail,com>
2025-06-09 13:01:06 +0200

    "updated"

--- image_filters.py
+++ image_filters.py
@@ -3,7 +3,7 @@ import math
 
 class ImageFilters:
     @staticmethod
-    def gaussian_kernel(size, sigma=0.5):
+    def gaussian_kernel(size, sigma=1.0):
         kernel = np.zeros((size, size), dtype=np.float32)
         center = size // 2
         for i in range(size):
@@ -29,3 +29,23 @@ class ImageFilters:
     def gaussian_blur(image, size=5, sigma=1.0):
         kernel = ImageFilters.gaussian_kernel(size, sigma)
         return ImageFilters.apply_kernel(image, kernel)
+
+    @staticmethod
+    def median_filter(image, size=3):
+        h, w = image.shape
+        k = size // 2
+        output = np.zeros_like(image, dtype=np.uint8)
+        for i in range(k, h - k):
+            for j in range(k, w - k):
+                region = image[i - k:i + k + 1, j - k:j + k + 1]
+                output[i, j] = np.median(region)
+        return output
+
+    @staticmethod
+    def histogram_equalization(image):
+        hist, _ = np.histogram(image.flatten(), bins=256, range=(0, 256))
+        cdf = hist.cumsum()
+        cdf_masked = np.ma.masked_equal(cdf, 0)
+        cdf_normalized = (cdf_masked - cdf_masked.min()) * 255 / (cdf_masked.max() - cdf_masked.min())
+        cdf_filled = np.ma.filled(cdf_normalized, 0).astype(np.uint8)
+        return cdf_filled[image]
--- mian.py
+++ mian.py
@@ -2,13 +2,16 @@ from images import ImageLoader
 from image_filters import ImageFilters
 from PIL import Image
 
-# Wczytanie obrazu
+# Wczytaj obraz
 loader = ImageLoader("obraz.bmp")
 img = loader.load_grayscale()
 
 # Filtry
-blurred = ImageFilters.gaussian_blur(img, size=5, sigma=10)
-
+blurred = ImageFilters.gaussian_blur(img, size=5, sigma=1.0)
+medianed = ImageFilters.median_filter(img, size=3)
+equalized = ImageFilters.histogram_equalization(img)
 
 # Zapis wyników
 Image.fromarray(blurred).save("gauss.png")
+Image.fromarray(medianed).save("median.png")
+Image.fromarray(equalized).save("equalized.png")
