cbca8ef59b98db77080e3abcc236df5f6bccaad1
PiotrStanzecki <piotr.stanzecki@gmail.com>
2025-06-10 16:01:07 +0200

    final changes

--- image_filters.py
+++ image_filters.py
@@ -1,41 +1,43 @@
 import numpy as np
 import math
+from scipy.ndimage import convolve
 
 class ImageFilters:
+  
+
     @staticmethod
     def gaussian_kernel(size, sigma=1.0):
-        kernel = np.zeros((size, size), dtype=np.float32)
+ 
+        if size % 2 == 0:
+            raise ValueError("Kernel size must be an odd number.")
+
         center = size // 2
-        for i in range(size):
-            for j in range(size):
-                x = i - center
-                y = j - center
-                kernel[i, j] = math.exp(-(x**2 + y**2) / (2 * sigma**2))
+
+        x = np.arange(-center, center + 1)
+        y = np.arange(-center, center + 1)
+
+        X, Y = np.meshgrid(x, y)
+
+        kernel = np.exp(-(X**2 + Y**2) / (2 * sigma**2))
+
         return kernel / np.sum(kernel)
 
     @staticmethod
     def apply_kernel(image, kernel):
-        h, w = image.shape
-        k = kernel.shape[0] // 2
-        output = np.zeros_like(image, dtype=np.uint8)
-        for i in range(k, h - k):
-            for j in range(k, w - k):
-                region = image[i - k:i + k + 1, j - k:j + k + 1]
-                value = np.sum(region * kernel)
-                output[i, j] = np.clip(value, 0, 255)
-        return output
+        convolved_image = convolve(image.astype(np.float32), kernel, mode='constant', cval=0.0)
+        return np.clip(convolved_image, 0, 255).astype(np.uint8)
 
 
     @staticmethod
     def gaussian_blur(image, size=5, sigma=1.0):
         kernel = ImageFilters.gaussian_kernel(size, sigma)
         return ImageFilters.apply_kernel(image, kernel)
-    
+
     @staticmethod
     def edge_detection(image, force):
-        kernel_edge = np.array([[0,  force, 0],[force, -4*force, force],[0,  force, 0]], dtype=np.float32)
+        kernel_edge = np.array([[0,  force, 0], [force, -4 * force, force], [0,  force, 0]], dtype=np.float32)
         return ImageFilters.apply_kernel(image, kernel_edge)
-    
+
     @staticmethod
     def median_filter(image, size=3):
         h, w = image.shape
@@ -55,3 +57,4 @@ class ImageFilters:
         cdf_normalized = (cdf_masked - cdf_masked.min()) * 255 / (cdf_masked.max() - cdf_masked.min())
         cdf_filled = np.ma.filled(cdf_normalized, 0).astype(np.uint8)
         return cdf_filled[image]
+
